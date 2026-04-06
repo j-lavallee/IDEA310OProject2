d2d09101a641ac184760b755fb38f058aa297563
John LaVallee <lavalljm@colostate.edu>
2026-04-06 16:43:50 -0600

    Messing with ragdolls

---
+++ Assets/MyStuff/Interactor.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interactor : MonoBehaviour
+{
+    public LayerMask interactableLayermask;
+    Interactable interactable;
+    public Image interactImage;
+    public Sprite defaultIcon;
+    public Vector2 defaultIconSize;
+    public Sprite defaultInteractIcon;
+    public Vector2 defaultInteractIconSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 4, interactableLayermask))
+        {
+            if(hit.collider.GetComponent<Interactable>() != false)
+            {
+                if(interactable == null || interactable.ID != hit.collider.GetComponent<Interactable>().ID)
+                {
+                    interactable = hit.collider.GetComponent<Interactable>();
+                }
+
+                if(interactable.interactIcon != null)
+                {
+                    interactImage.sprite = interactable.interactIcon;
+                    if(interactable.iconSize == Vector2.zero)
+                    {
+                        interactImage.rectTransform.sizeDelta = defaultInteractIconSize;
+                    }
+                    else
+                    {
+                        interactImage.rectTransform.sizeDelta = interactable.iconSize;
+                    }
+                }
+                else
+                {
+                    interactImage.sprite = defaultInteractIcon;
+                    interactImage.rectTransform.sizeDelta = defaultInteractIconSize;
+                }
+                if(Input.GetKeyDown(KeyCode.E))
+                {
+                    interactable.onInteract.Invoke();
+                }
+            }
+        }
+        else
+        {
+            if(interactImage.sprite != defaultIcon)
+            {
+                interactImage.sprite = defaultIcon;
+                interactImage.rectTransform.sizeDelta = defaultIconSize;
+            }
+        }
+    }
+}
