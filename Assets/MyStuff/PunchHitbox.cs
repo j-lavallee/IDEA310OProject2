60bd261db72a2b182ab23247a8aa6e1b8adc410a
John LaVallee <lavalljm@colostate.edu>
2026-04-15 17:01:20 -0600

    PUNCHY PUNCHY

--- Assets/MyStuff/PunchHitbox.cs
+++ Assets/MyStuff/PunchHitbox.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class PunchHitbox : MonoBehaviour
 {
     public GameObject ragdoll;
-
+    public float force;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("ohko"))
         {
             Debug.Log("hit");
-            Instantiate(ragdoll, transform.position, transform.rotation);
             Destroy(other.gameObject);
-            ragdoll.GetComponentInChildren<Rigidbody>().AddForce(transform.forward * 10);
+            var rg = Instantiate(ragdoll, other.transform.position, other.transform.rotation);
+
+            foreach (Rigidbody bone in rg.GetComponentsInChildren<Rigidbody>()){
+                bone.AddForce(transform.forward * force, ForceMode.Impulse);
+            }
         }
     }
 }
